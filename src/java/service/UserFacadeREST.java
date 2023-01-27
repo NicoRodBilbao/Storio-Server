b16ad7f0b3c24fa26d4b62ed150051dedf16628b
joana-renteria <joana.rente6@tuta.io>
2023-01-27 14:17:56 +0100

    clean imports

--- src/java/service/UserFacadeREST.java
+++ src/java/service/UserFacadeREST.java
@@ -1,6 +1,5 @@
 package service;
 
-import com.sun.mail.imap.IMAPBodyPart;
 import entities.User;
 import entities.UserPrivilege;
 import entities.UserStatus;
@@ -8,10 +7,7 @@ import exceptions.CreateException;
 import exceptions.FindException;
 import exceptions.RemoveException;
 import exceptions.UpdateException;
-import java.time.Duration;
-import static java.time.Instant.now;
 import java.util.List;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -37,18 +33,6 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 import javax.ws.rs.core.Response;
 
 /**
